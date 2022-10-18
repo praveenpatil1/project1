7f0cfb95ffb8824d0abbb1168cda9a82c189cad1
Praveen <Praveen@DESKTOP-F6BB92T>
2022-10-18 09:39:18 +0530

    Basic Seleniuum

---
+++ src/main/java/pomPages/SkillraryDemoLogin.java
@@ -0,0 +1,20 @@
+package pomPages;
+
+public class SkillraryDemoLogin 
+{
+	@FindBy (id="course")
+	private WebElement coursetab;
+	
+	@FindBy (xpath="//a[text()='Seleniumm Training']")
+	private WebElement seleniumtraining;
+	
+	public WebElement getCoursedd()
+	{
+		return coursedd;
+	}
+	@FindBy(name-"addressttype")
+	private WebElement coursedd;
+	
+	public SkillraryDemoLogin(WebDriver)
+
+}
